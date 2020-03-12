41a621679c7dcc57cfd068bfad3fb8bbe6b8d3d9
cg2lab <cg2lab@DESKTOP-HCES3GB>
2020-03-12 20:21:40 +0800

    加入專案檔案。

---
+++ demo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace demo
+{
+    class Program
+   static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+        int 冷氣卡 = 1000;
+
+        while (true)
+        {
+            
+            if (冷氣卡 <= 0)
+            {
+                Console.WriteLine("請重新儲值...");
+                break;
+            }
+
+            //檢查是否有人開冷氣
+            int 每次花費 = 0;
+            string attackString;
+            attackString = Console.ReadLine();
+            每次花費 = int.Parse(attackString);
+            //Console.WriteLine(attack);
+
+            //扣血
+            冷氣卡 = hp - attack;
+
+        }
+        Console.ReadLine();
+    }
+}
